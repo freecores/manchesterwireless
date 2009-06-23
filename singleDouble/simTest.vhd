1118f40e1cf9572b102e5d9d9958dc271e12d88d
kingmu <kingmu@77c073ab-e474-4067-98f6-7ea5aedfc689>
2009-06-23 02:20:55 +0000

    Added new singleDouble files

--- singleDouble/simTest.vhd
+++ singleDouble/simTest.vhd
@@ -9,7 +9,18 @@ end test_sim;
 
 architecture Behavioral of test_sim is
 
-	COMPONENT singleDouble
+	COMPONENT singleDouble_original
+	PORT(
+    clk_i   :  in  std_logic;
+    ce_i    :  in  std_logic;    
+    rst_i   :  in  std_logic;
+    data_i  :  in  std_logic;
+    q_o     :  out std_logic_vector(3 downto 0);
+    ready_o :  out std_logic
+		);
+	END COMPONENT;
+
+  COMPONENT singleDouble
 	PORT(
     clk_i   :  in  std_logic;
     ce_i    :  in  std_logic;    
@@ -23,30 +34,41 @@ architecture Behavioral of test_sim is
   signal clk : std_logic := '0';
   signal ce_i : std_logic := '0';
   signal mdi : std_logic := '0';
-  signal q : std_logic_vector(3 downto 0);
-  signal nd : std_logic;
+  signal q_orig : std_logic_vector(3 downto 0);
+  signal nd_orig : std_logic;
+  signal q_modified : std_logic_vector(3 downto 0);
+  signal nd_modified : std_logic;
   
   constant period : time := 10 ns;
   constant md_period : time := period*16;
   signal reset : std_logic := '1';
 begin
 
-	Inst_singleDouble: singleDouble PORT MAP(
+	Inst_orig: singleDouble_original PORT MAP(
     clk_i =>  clk,
     ce_i  =>  ce_i,
     rst_i  =>  reset,
     data_i   =>  mdi,
-    q_o     =>  q,
-    ready_o    =>  nd
+    q_o     =>  q_orig,
+    ready_o    =>  nd_orig
 	);
-  
+
+  Inst_modified: singleDouble PORT MAP(
+    clk_i =>  clk,
+    ce_i  =>  ce_i,
+    rst_i  =>  reset,
+    data_i   =>  mdi,
+    q_o     =>  q_modified,
+    ready_o    =>  nd_modified
+  );
+
   process
   begin
     loop
       reset <= '1';
       ce_i <= '0';
       
-      wait for 2*md_period;
+      wait for (2*md_period);
     
       reset <= '0';
       ce_i <= '1';
